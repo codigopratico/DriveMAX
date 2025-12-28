30b28665af80a9d6595199b9208e75b4e55e8b77
Kauan Gabriel <fandaart.75@gmail.com>
2025-12-28 20:03:49 -0300

    Ajustando tamanho das imagens da seção Segurança v1.1

--- src/components/seguranca/IconesView.vue
+++ src/components/seguranca/IconesView.vue
@@ -55,7 +55,7 @@ const icones = ref([
         color: var(--color-preto);
     }
     img{
-        height: clamp(100px, 4vh, 160px);
+        height: clamp(120px, 4vh, 160px);
         width: fit-content;
     }
 
